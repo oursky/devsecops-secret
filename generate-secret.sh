04324c6160feace513fcca9e648fe57d984cb858
hidden-dimensions <20926615+hidden-dimensions@users.noreply.github.com>
2019-07-25 13:46:29 +0800

    support pipe to/from stdio #7

--- generate-secret.sh
+++ generate-secret.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
-ARGS_IN=
-ARGS_OUT=.env
+ARGS_IN=/dev/stdin
+ARGS_OUT=
 ARGS_KEYWORD=GENERATE_SECRET
 ARGS_SECRET_LENGTH=48
 
 function usage {
     echo "usage: generate-secret.sh -i .env.in [-o .env] [-k keyword] [-s strength]"
-    echo "  -i .env.in     Input template file"
-    echo "  -o .env        Output file, default to .env"
+    echo "  -i .env.in     Input template file, default to stdin"
+    echo "  -o .env        Output file, default to stdout"
     echo "  -k keyword     Keyword for secret, default to GENERATE_SECRET"
     echo "  -s strength    Secret strength, default 48 bytes before base64, might be less due to removing special characters."
 }
@@ -79,13 +79,15 @@ function generate_secret {
     # backup
     if [ -f "${OUTFILE}" ]; then
         cp -f "${OUTFILE}" "${OUTFILE}.bak"
+        mv "${TMPFILE}" "${OUTFILE}"
+    else
+        cat "${TMPFILE}" && rm -f "${TMPFILE}"
     fi
-    mv "${TMPFILE}" "${OUTFILE}"
 }
 function main {
     echo "== Secret Generator =="
     parse_arguments $@
-    if [ -z ${ARGS_IN} ] || [ -z ${ARGS_OUT} ] || [ -z ${ARGS_KEYWORD} ] || [ -z ${ARGS_SECRET_LENGTH} ]; then
+    if [ -z ${ARGS_IN} ] || [ -z ${ARGS_KEYWORD} ] || [ -z ${ARGS_SECRET_LENGTH} ]; then
         usage
         exit 0
     fi
